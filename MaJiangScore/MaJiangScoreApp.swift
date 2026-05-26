1ae6536412b71fa429aca6d490ce0a8dd81c39c7
mh1te-att <zyc4fun@outlook.com>
2026-05-26 18:30:33 +0800

    Initial Commit

---
+++ MaJiangScore/MaJiangScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaJiangScoreApp.swift
+//  MaJiangScore
+//
+//  Created by Yingchu Zhao on 2026/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MaJiangScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
